fb7013e47bd135e0e93641e726332e78c15948be
Juan Lopez <jcamlopezmarin@gmail.com>
2025-09-12 10:34:44 -0500

    estrcutura v2

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -1,11 +1,4 @@
 import mysql from 'mysql2/promise';
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-});
-
 export const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
